d606bffd539f2835c4a51c59e097ceb41516b658
Iam-Tej23 <165941516+Iam-Tej23@users.noreply.github.com>
2025-07-02 00:06:01 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -2,7 +2,7 @@ def call(){
  echo"Pushing the image to dockerHub"
  withCredentials([usernamePassword(credentialsId:"dockerHub",passwordVariable:"DOCKER_PASS",usernameVariable:"DOCKER_USER")]){
  sh "docker login -u ${env.DOCKER_USER} -p ${env.DOCKER_PASS}"
- sh "sudo docker image tag notes-app:latest ${env.DOCKER_USER}/notes-app:latest"
+ sh "docker image tag notes-app:latest ${env.DOCKER_USER}/notes-app:latest"
  sh "sudo docker push ${env.DOCKER_USER}/notes-app:latest"
                 }
 }
